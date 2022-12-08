bc607e53c759b96d554ca911b4e1f77a5f8496bc
Nicholas Otto <119625865+nicholasotto@users.noreply.github.com>
2022-12-07 21:05:35 -0500

    Add files via upload

--- ButtonCheck.v
+++ ButtonCheck.v
@@ -1,11 +1,11 @@
-module ButtonCheck(input clk, rst, en, input [2:0]val, input [7:0] click, output reg done);
+module ButtonCheck(input clk, rst, en, input [2:0]val, input [7:0] click, output reg done, output reg correct);
 
 reg [2:0] S; reg [2:0] NS;
 parameter start = 3'd0,
 			valCheck = 3'd1,
 			waiting = 3'd2,
 			right = 3'd3,
-			wrong = 3'd4;
+			exit = 3'd4;
 			
 parameter a = 8'b00000001,
 			b = 8'b00000010,
@@ -39,25 +39,26 @@ always @(*) begin
 		else begin
 			case(val)
 				acheck: begin if (click == a) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 				bcheck : begin if (click == b) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 				selcheck: begin if (click == sel) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 				upcheck :begin if (click == up) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 				downcheck : begin if (click == down) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 				leftcheck :begin if (click == left) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 				rightcheck :begin if (click == bright) NS <= right;
-				else NS <= wrong; end
+				else NS <= exit; end
 			
 			endcase end
 		
 		end
-		right: NS <= start;
-		wrong: NS <= start;
+		right: NS <= exit;
+		exit: begin if (en == 1'b1) NS <= exit;
+		else NS <= start; end
 	
 	
 	endcase
@@ -67,12 +68,14 @@ end
 always @(posedge clk or negedge rst) begin
 	if (rst == 1'b0) begin
 		done = 1'b0;
-	
+		correct = 1'b0;
 	end else begin
 		case (S) 
 			start: begin
-			done = 1'b0; end
-			right: done = 1'b1;
+			done = 1'b0; 
+			correct = 1'b0; end
+			right: begin correct = 1'b1; end
+			exit: begin done = 1'b1; correct = 1'b0; end
 		endcase
 	end
 	
